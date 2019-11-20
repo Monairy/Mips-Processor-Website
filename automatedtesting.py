fe4270768158a9b7263f4f504d0dc0a8ecb0df1f
Ahmed EL-Monairy <42189296+Monairy@users.noreply.github.com>
2019-11-21 00:29:52 +0200

    Update automatedtesting.py

--- automatedtesting.py
+++ automatedtesting.py
@@ -4,6 +4,7 @@ import subprocess
 import filecmp
 from termcolor import colored
 
+
 testcases='testcases/'
 InputToAssembler='assembly.txt'
 
@@ -24,9 +25,30 @@ for filename in listdir(testcases): #get all contents of testcases folder
             print "[+] testcase " + filename + colored(" RegFile",'yellow')+ " contents" + colored(' succeeded','green')
         else:
             print "[+] testcase " + filename + colored(" RegFile",'yellow')+ " contents"+ colored(' failed','red')
-            
+            with open('_FromRegFile.txt','r') as regfile:
+             regcontent=regfile.readlines()
+             with open('testcases/Register_Output/'+filename) as testcaseregfile:
+                CorrectReg=testcaseregfile.readlines()
+                for i in range (0,len(regcontent)):
+                     if (regcontent[i]!=CorrectReg[i]):
+                         print "[*]"+colored("Error in line # "+str(i),'white',"on_red")
+                         print  colored(regcontent[i].split('\n')[0],'red')
+                         print "vs"
+                         print colored(CorrectReg[i].split('\n')[0],'green')
         mem_compare=filecmp.cmp('_FromDataMem.txt','testcases/DataMem_Output/'+filename) # compare datamemory files
         if (str(mem_compare)=="True"):
             print "[-] testcase " + filename + colored(" DataMem",'cyan')+ " contents" + colored(' succeeded','green')
         else:
             print "[-] testcase " + filename + colored(" DataMem",'cyan')+ " contents"+ colored(' failed','red')
+            with open('_FromDataMem.txt','r') as datamemfile:
+               datacontent=datamemfile.readlines()
+               with open('testcases/DataMem_Output/'+filename) as testcasedatafile:
+                  CorrectData=testcasedatafile.readlines()
+                  for x in range (0,len(CorrectData)):
+                     if (datacontent[x]!=CorrectData[x]):
+                         print "[*]"+colored("Error in line # "+str(x),'white',"on_red")
+                         print  colored(datacontent[x].split('\n')[0],'red')
+                         print "vs"
+                         print colored(CorrectData[x].split('\n')[0],'green')
+
+        print colored("=========================================================",'white','on_yellow')                 
