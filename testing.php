a075ac82da6d5e5b36f02231889d6d46c79c728c
Ahmed EL-Monairy <42189296+Monairy@users.noreply.github.com>
2019-11-29 12:46:36 +0200

    Add files via upload
    automated testing

---
+++ testing.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>MIPS Processor</title>
+</head>
+<form action="index.php" method="get">
+<input type="submit" value="back" style="height:50px;width:228px;">
+</form>
+<body>
+<pre>
+<b>
+<div id="first">
+<?php
+exec('C:\Python27\python.exe testing.py > testing.txt');
+echo file_get_contents("testing.txt");
+?>
+</div>
+</b>
+<style>
+#first{
+left:35%;
+position:absolute;
+
+</style>
+
+</p>
+</body>
+</html>
